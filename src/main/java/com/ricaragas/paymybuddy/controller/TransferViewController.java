3f050b2179296bb2866798593c569ef6bf2f2402
ricar <ricaragas@hotmail.com>
2022-08-10 16:31:27 +0200

    Using @WithMockUser for the unit tests of controllers

--- src/main/java/com/ricaragas/paymybuddy/controller/TransferViewController.java
+++ src/main/java/com/ricaragas/paymybuddy/controller/TransferViewController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class TransferViewController {
@@ -14,8 +17,13 @@ public class TransferViewController {
         var viewName = "transfer";
         var model = new HashMap<String, Object>();
 
-        model.put("contacts", null);
-        model.put("transfers", null);
+        var contacts = new ArrayList<>(List.of("Contact1", "Contact2"));
+
+        var transfers = new ArrayList<HashMap<String,String>>();
+        transfers.add(new HashMap<>(Map.of("Col1","Value1","Col2","Value2","Col3","Value3")));
+
+        model.put("contacts", contacts);
+        model.put("transfers", transfers);
 
         return new ModelAndView(viewName, model);
     }
--- src/test/java/com/ricaragas/paymybuddy/unit/TransferViewControllerTest.java
+++ src/test/java/com/ricaragas/paymybuddy/unit/TransferViewControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,11 +19,11 @@ public class TransferViewControllerTest {
     MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     public void when_get_then_success() throws Exception {
         mockMvc.perform(get("/transfer"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(view().name("transfer"))
-                .andExpect(model().size(1))
                 .andExpect(model().attributeExists("contacts"))
                 .andExpect(model().attributeExists("transfers"));
     }
