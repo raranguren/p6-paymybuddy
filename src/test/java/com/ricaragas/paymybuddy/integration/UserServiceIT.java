27efb6b13a36532860a68392f851d2ccd181a056
ricar <ricaragas@hotmail.com>
2022-08-10 15:59:52 +0200

    using profile "test" to run the integration tests in the test database

--- src/test/java/com/ricaragas/paymybuddy/integration/UserServiceIT.java
+++ src/test/java/com/ricaragas/paymybuddy/integration/UserServiceIT.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class UserServiceIT {
 
     @Autowired
