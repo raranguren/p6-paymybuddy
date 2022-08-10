7f15489992a378185f53036c75621ce486d3ff4b
ricar <ricaragas@hotmail.com>
2022-08-10 13:03:43 +0200

    enabling logout and remember-me features

--- src/main/java/com/ricaragas/paymybuddy/configuration/SpringSecurityConfig.java
+++ src/main/java/com/ricaragas/paymybuddy/configuration/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 // @EnableWebSecurity deprecated in Spring Boot 2.7
@@ -16,13 +17,26 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeRequests()
-                .antMatchers("/styles/*", "/icons/*").permitAll()
+        httpSecurity.authorizeRequests()
+                .antMatchers("/login", "/styles/*.css", "/icons/*.svg")
+                .permitAll()
                 .anyRequest().authenticated()
+
+                .and().formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .defaultSuccessUrl("/")
+
                 .and()
-                .formLogin().usernameParameter("email").defaultSuccessUrl("/")
-                .loginPage("/login").permitAll();
+                .rememberMe()
+
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                ;
+
         return httpSecurity.build();
     }
 
