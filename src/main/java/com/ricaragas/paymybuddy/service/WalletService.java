65a9a0b039f3fb8506c180bebf2a44dc1fa78322
ricar <ricaragas@hotmail.com>
2022-08-11 13:55:33 +0200

    test for WalletService::getWalletForAuthenticatedUser

--- src/main/java/com/ricaragas/paymybuddy/service/WalletService.java
+++ src/main/java/com/ricaragas/paymybuddy/service/WalletService.java
@@ -13,7 +13,6 @@ public class WalletService {
 
     @Autowired
     UserService userService;
-
     @Autowired
     WalletRepository walletRepository;
     public Optional<Wallet> getWalletForUser(User user) {
--- src/test/java/com/ricaragas/paymybuddy/unit/WalletServiceTest.java
+++ src/test/java/com/ricaragas/paymybuddy/unit/WalletServiceTest.java
@@ -3,6 +3,7 @@ package com.ricaragas.paymybuddy.unit;
 import com.ricaragas.paymybuddy.model.User;
 import com.ricaragas.paymybuddy.model.Wallet;
 import com.ricaragas.paymybuddy.repository.WalletRepository;
+import com.ricaragas.paymybuddy.service.UserService;
 import com.ricaragas.paymybuddy.service.WalletService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class WalletServiceTest {
@@ -23,6 +24,9 @@ public class WalletServiceTest {
 
     @Mock
     WalletRepository walletRepository;
+
+    @Mock
+    UserService userService;
 
     @Test
     public void when_get_wallet_by_user_then_success() {
@@ -46,6 +50,21 @@ public class WalletServiceTest {
         var result = walletService.getWalletForUser(user);
         // ASSERT
         assert(result.isEmpty());
+    }
+
+    @Test
+    public void when_get_wallet_for_logged_user_then_success() {
+        // ARRANGE
+        var user = new User();
+        var wallet = new Wallet();
+        user.setWallet(wallet);
+        when(userService.getAuthenticatedUser()).thenReturn(Optional.of(user));
+        // ACT
+        var result = walletService.getWalletForAuthenticatedUser();
+        // ASSERT
+        assert(result.isPresent());
+        assertEquals(wallet, result.get());
+        verifyNoInteractions(walletRepository);
     }
 
 }
