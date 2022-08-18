5dc8e83267fc882f0656c31271fa81d8b4e9cb8f
ricar <ricaragas@hotmail.com>
2022-08-18 20:12:47 +0200

    removing unused method

--- src/main/java/com/ricaragas/paymybuddy/repository/WalletRepository.java
+++ src/main/java/com/ricaragas/paymybuddy/repository/WalletRepository.java
@@ -1,13 +1,8 @@
 package com.ricaragas.paymybuddy.repository;
 
-import com.ricaragas.paymybuddy.model.User;
 import com.ricaragas.paymybuddy.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
-
-    Optional<Wallet> findByUser(User user);
 }
